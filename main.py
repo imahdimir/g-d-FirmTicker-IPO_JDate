1ee3e27e73cbf23f274f5d29d47d1e719e152213
imahdimir <imahdimir@gmail.com>
2022-09-10 01:37:39 +0430

    v3

--- main.py
+++ main.py
@@ -1,59 +1,63 @@
 """
 
-
   """
-##
+
+import json
 
 from githubdata import GithubData
-from mirutil import utils as mu
-from mirutil.df_utils import read_data_according_to_type as read_data
-from mirutil.df_utils import save_as_prq_wo_index as save_as_prq
-from persiantools.jdatetime import JalaliDate
 
 
-targ_url = 'imahdimir/d-firms-IPO-date'
+class GDUrl :
+    with open('gdu.json' , 'r') as fi :
+        gj = json.load(fi)
 
-cipojd = 'IPO_JDate'
-cftic = 'FirmTicker'
-cdt = 'Date'
+    cur = gj['cur']
+    trg = gj['trg']
+
+gu = GDUrl()
+
+class ColName :
+    ipojd = 'IPO_JDate'
+    ftic = 'FirmTicker'
+
+c = ColName()
 
 def main() :
-  pass
-  ##
-  rp_ipo = GithubData(targ_url)
-  rp_ipo.clone()
-  ##
-  dipp = rp_ipo.data_fp
-  dip = read_data(dipp)
-  ##
-  dip[cdt] = dip[
-    cipojd].apply(lambda x : JalaliDate.fromisoformat(x).to_gregorian())
-  ##
-  assert dip[cftic].is_unique
-  ##
-  save_as_prq(dip , dipp)
-  ##
-  tokp = '/Users/mahdi/Dropbox/tok.txt'
-  tok = mu.get_tok_if_accessible(tokp)
-  ##
-  msg = 'for duplicated firm tickers kept the earlier date'
-  rp_ipo.commit_and_push(msg , user = rp_ipo.user_name , token = tok)
+    pass
 
-  ##
+    ##
 
+    gd = GithubData(gu.trg)
+    gd.overwriting_clone()
+    ##
+    df = gd.read_data()
+    ##
 
-  rp_ipo.rmdir()
+    assert df[c.ftic].is_unique
+    ##
+
+    fp = gd.data_fp
+    df.to_csv(fp , index = False)
+    ##
+    msg = 'gov by: '
+    msg += gu.cur
+    ##
+
+    gd.commit_and_push(msg)
+
+    ##
 
 
-  ##
+    gd.rmdir()
 
 
-  ##
+    ##
+
+
+    ##
 
 ##
-
-
 if __name__ == '__main__' :
-  main()
+    main()
 
 ##
